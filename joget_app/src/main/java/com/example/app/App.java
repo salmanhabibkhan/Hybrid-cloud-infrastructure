55b0900736784dd52f2d50a2a261b819e6fc8908
SALMAN HABIBKHAN <salmanhabib220@gmail.com>
2025-08-08 14:36:53 +0500

    Inital commit

---
+++ joget_app/src/main/java/com/example/app/App.java
@@ -0,0 +1,33 @@
+package com.example.app;
+
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/joget_db";
+        String dbUser = "root";
+        String dbPassword = "salman123"; // change this
+
+        try {
+
+            Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+            System.out.println("Connected to MySQL!");
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()) {
+                System.out.println(
+                    resultSet.getInt("id") + " | " +
+                    resultSet.getString("name") + " | " +
+                    resultSet.getString("email")
+                );
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Error connecting to MySQL:");
+            e.printStackTrace();
+        }
+    }
+}
